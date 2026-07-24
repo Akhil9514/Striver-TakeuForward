60baaafb44c3742ac300c97214b0d41e0d31d31b
Akhil9514 <vibhakarakhil@gmail.com>
2026-07-24 13:25:55 +0530

    Sorting Started

---
+++ Sorting/SelectionSort.cs
@@ -0,0 +1,20 @@
+public class SelectionSort
+{
+    public static void Run()
+    {
+        int[] arr = {7,4,1,5,3};
+        int maxIndex = 0;
+        int largest = arr[maxIndex];
+
+        for (int i=1; i<arr.Length; i++)
+        {
+            
+
+        }
+
+        foreach(var item in arr)
+        {
+            Console.WriteLine(item);
+        }
+    }
+}
