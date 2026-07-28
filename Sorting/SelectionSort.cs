9e22cd2d34fdf3709d9024ee65097f1d2d947ffd
Akhil9514 <vibhakarakhil@gmail.com>
2026-07-28 13:50:22 +0530

    Selection Sort done

--- Sorting/SelectionSort.cs
+++ Sorting/SelectionSort.cs
@@ -3,12 +3,26 @@ public class SelectionSort
     public static void Run()
     {
         int[] arr = {7,4,1,5,3};
-        int maxIndex = 0;
-        int largest = arr[maxIndex];
+        int largestIndex;
 
-        for (int i=1; i<arr.Length; i++)
+        for (int i=0; i<arr.Length-1; i++)
         {
-            
+            largestIndex = i;
+
+            for (int j=i+1; j<arr.Length; j++)
+            {
+
+                if (arr[j] >= arr[largestIndex])
+                {
+                    largestIndex = j;
+                }
+
+            }
+
+            int temp = arr[i];
+            arr[i] = arr[largestIndex];
+            arr[largestIndex] = temp;
+
 
         }
 
